4cec448fc74fec4ae71d5b3edd686ed9632148f7
Arpit <arpit@interfacem.com>
2020-08-08 01:39:07 +0530

    - Initial setup - Created server.js [In progess]

---
+++ server.js
@@ -0,0 +1,31 @@
+// Setup code to get the server running
+
+// Create an express server
+const express = require('express');
+
+// Variable running that express function
+const app = express();
+
+// Get a server to be used with socket.io
+const server = require('http').Server(app);
+
+const io = require('socket.io')(server);
+const { v4: uuidV4} = require('uuid');
+
+// Setup express server
+app.set('view engine', 'ejs');
+
+//Setup static folder. All javascript & css will be put in public.
+app.use(express.static('public'));
+
+//This will take a request and a response and create a new room and redirect user to that room
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidV4()}`)
+});
+
+//Route for room
+app.get('/:room', (rew, res) => {
+   res.render('room', {roomId: req.params.room});
+});
+
+server.listen(3000);
